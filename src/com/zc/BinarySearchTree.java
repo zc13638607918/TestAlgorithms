7009c9582d72c228b3bd52ce21eff3776490c5c5
zc13638607918 <1418269209@qq.com>
2019-11-18 20:30:19 +0800

    第三次修改，提交了二叉搜索树的删除节点的正式版本

--- src/com/zc/BinarySearchTree.java
+++ src/com/zc/BinarySearchTree.java
@@ -3,7 +3,7 @@ package com.zc;
 public class BinarySearchTree {
     Node<Integer> root;
 
-    BinarySearchTree(Node<Integer> root) {//二叉搜索树
+    BinarySearchTree(Node<Integer> root) {
         this.root = root;
     }
 
@@ -21,27 +21,37 @@ public class BinarySearchTree {
         System.out.println("初始二叉树：");
         ergodic.order("中序遍历");
 
-        System.out.println("删除根节点\"5\"：");
+        System.out.println("删除节点\"5\"：");
         bst.delete(5);
         Ergodic ergodic1 = new Ergodic(bst.root);
         ergodic1.order("中序遍历");
 
-        System.out.println("删除叶子节点\"7\":");
+        System.out.println("删除节点\"4\":");
+        bst.delete(4);
+        ergodic1.order("中序遍历");
+
+        System.out.println("删除节点\"7\":");
         bst.delete(7);
         ergodic1.order("中序遍历");
 
-        System.out.println("删除无右子树的节点\"8\":");
+        System.out.println("删除节点\"8\":");
         bst.delete(8);
         ergodic1.order("中序遍历");
 
-        System.out.println("删除无左子树的节点\"6\":");
+        System.out.println("删除节点\"6\":");
         bst.delete(6);
         Ergodic ergodic2 = new Ergodic(bst.root);
         ergodic2.order("中序遍历");
 
-        System.out.println("删除有左右子树的节点\"2\":");
+        System.out.println("删除节点\"2\":");
         bst.delete(2);
-        ergodic2.order("中序遍历");
+        Ergodic ergodic3 = new Ergodic(bst.root);
+        ergodic3.order("中序遍历");
+
+        System.out.println("删除节点\"3\":");
+        bst.delete(3);
+        Ergodic ergodic4 = new Ergodic(bst.root);
+        ergodic4.order("中序遍历");
 
     }
 
@@ -97,57 +107,48 @@ public class BinarySearchTree {
         Node successor = findSuccessor(node);//找到后继节点
         Node fatherOfSuccessor;//后继节点的父亲
 
-        if (successor == null) {//
-            if (node == this.root) {
+        //如被删节点无后继，说明该节点无右子树
+        if (successor == null) {
+            if (node == this.root) {//且被删节点为根时（无父节点）
                 this.root = node.getLeft();
                 node.setLeft(null);
                 return;
             }
-            if (node.getLeft() == null) {
-                if (isLeft(node)) {
-                    fatherOfNode.setLeft(null);
-                } else fatherOfNode.setRight(null);
-            } else {
-                fatherOfNode.setRight(node.getLeft());
-            }
+            if (isLeft(node)) {//无后继且被删节点不为根时需连接爷爷与孙子
+                fatherOfNode.setLeft(node.getLeft());
+            } else fatherOfNode.setRight(node.getLeft());
             return;
         }
+
+        //如被删节点有后继，处理后继节点遗留关系
         fatherOfSuccessor = getFather(successor);
-        //处理后继节点遗留关系
-        if (successor.getRight() == null) {
-            if (isLeft(successor)) {
-                fatherOfSuccessor.setLeft(null);
-            } else fatherOfSuccessor.setRight(null);
-        } else {
-            if (isLeft(successor)) {
-                fatherOfSuccessor.setLeft(successor.getRight());
-            } else fatherOfSuccessor.setRight(successor.getRight());
-        }
+        if (isLeft(successor)) {
+            fatherOfSuccessor.setLeft(successor.getRight());
+        } else fatherOfSuccessor.setRight(successor.getRight());
 
-        //后继节点继承被删节点
-
+        //后继节点继承被删节点关系
+        successor.setLeft(node.getLeft());
+        successor.setRight(node.getRight());
         if (fatherOfNode == null) {//被删节点为根节点
-            successor.setLeft(node.getLeft());
-            successor.setRight(node.getRight());
-            node.setLeft(null);
-            node.setRight(null);
             this.root = successor;
         } else {
-            successor.setLeft(node.getLeft());
-            successor.setRight(node.getRight());
             if (isLeft(node)) {
                 fatherOfNode.setLeft(successor);
             } else fatherOfNode.setRight(successor);
-            node.setLeft(null);
-            node.setRight(null);
         }
 
+        //销毁被删节点
+        node.setLeft(null);
+        node.setRight(null);
     }
 
-    private Node findSuccessor(Node node) {
-        return findSuccessor(node, node);
-    }
-
+    /**
+     * 找到node的后继节点并返回
+     *
+     * @param p    辅助节点，用于递归
+     * @param node 待寻找后继的节点
+     * @return
+     */
     private Node findSuccessor(Node p, Node node) {
         if (node.getRight() == null)
             return null;
@@ -160,43 +161,41 @@ public class BinarySearchTree {
         }
     }
 
-    private Node getFather(Node node) {
-        return getFather(this.root, node);
+    private Node findSuccessor(Node node) {
+        return findSuccessor(node, node);
     }
 
     /**
      * 取node的父节点
-     * @param p 辅助节点，用于递归
+     *
+     * @param p    辅助节点，用于递归
      * @param node 需取父亲的节点
      * @return
      */
     private Node getFather(Node p, Node node) {
         Node node1 = search((int) node.getData());
-        if (node1 == null) {
-            //System.out.println(node.getData()+"：该节点不在树中，无父节点！");
+        if (node1 == null) {//该节点不在树中，无父节点！
             return null;
-        } else if (node == this.root) {
-            //System.out.println(node.getData()+"：该节点为根节点，无父节点！");
+        } else if (node == this.root) {//该节点为根节点，无父节点！
             return null;
-        } else if ((int) node.getData() < (int) p.getData()) {
-            if (node == p.getLeft()) {
-                return p;
-            } else {
-                return getFather(p.getLeft(), node);
-            }
+        } else if (node == p.getLeft() | node == p.getRight()) {//找到父节点
+            return p;
+        } else if ((int) node.getData() < (int) p.getData()) {//递归
+            return getFather(p.getLeft(), node);
         } else {
-            if (node == p.getRight()) {
-                return p;
-            } else {
-                return getFather(p.getRight(), node);
-            }
+            return getFather(p.getRight(), node);
         }
+    }
+
+    private Node getFather(Node node) {
+        return getFather(this.root, node);
     }
 
     /**
      * 判断该节点是否为左节点
+     *
      * @param node 待判断的节点
-     * @return
+     * @return true为左，false为右
      */
     private boolean isLeft(Node node) {
         if (node == getFather(node).getLeft()) {
